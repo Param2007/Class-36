de9625c2d87f953de08e63512a7f7499a7bd7438
Param2007 <68640895+Param2007@users.noreply.github.com>
2020-08-31 16:55:56 +0530

    Add files via upload

---
+++ JS/game.js
@@ -0,0 +1,54 @@
+class Game {
+    
+    constructor() {
+
+    }
+
+    getState() {
+
+       var  gameStateRef = database.ref('gameState');
+
+       gameStateRef.on("value", (data) => {
+           gameState = data.val();
+       });
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+
+    async start() {
+        if(gameState === 0) {
+            player = new Player();
+
+            var playerCountRef = await database.ref('playerCount').once("value");
+
+            if(playerCountRef.exists()) {
+                playerCount = playerCountRef.val();
+
+                player.getCount();
+            }
+
+            form = new Form();
+
+            form.display();
+        }
+
+        car1 = createSprite(100,200);
+        car2 = createSprite(300,200);
+        car3 = createSprite(500,200);
+        car4 = createSprite(700,200);
+
+        cars = [car1, car2, car3, car4];
+    }
+
+    play() {
+
+    }
+
+    end() {
+        game.update(2);
+    }
+}
---
+++ JS/player.js
@@ -0,0 +1,27 @@
+class Player {
+
+    constructor() {
+
+        this.index = null;
+        this.name = null;
+        this.distance = 0;
+        this.rank = null;
+
+    }
+
+    getCount() {
+
+        var playerCountRef = database.ref('playerCount');
+
+        playerCountRef.on("value",(data) => {
+            playerCount = data.val();
+        });
+
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+}
